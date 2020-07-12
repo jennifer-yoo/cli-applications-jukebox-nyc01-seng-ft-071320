0b7894d50abb98369096a05d174ae016ae78925d
Jennifer Yoo <jennifer.yoo7@gmail.com>
2020-07-12 16:56:48 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -32,7 +32,7 @@ def play(songs)
 
   if songs.include?(user_input)
     puts "Playing #{user_input}"
-  elsif (1..9).include?(user_input)
+  elsif (1..9).include?(user_input.to_i)
     puts "Playing #{songs[user_input.to_i - 1]}"
   else
     puts "Invalid input, please try again"
